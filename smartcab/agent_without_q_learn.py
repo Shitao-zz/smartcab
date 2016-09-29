973ef4768ef7a8db19b5da853b743505732fa4a2
Shitao <steven_wst@hotmail.com>
2016-09-29 17:32:35 -0400

    project4 v2

--- smartcab/agent_without_q_learn.py
+++ smartcab/agent_without_q_learn.py
@@ -14,25 +14,11 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
-        # Trial statistics
-	self.trial = 0
+        self.trial = 0
 	self.total_reward = 0
 	self.negative_reward = 0
+        self.destination_reached = False
 	self.trial_stats = pd.DataFrame(columns=['total_reward','negative_reward','trial_length'])
-	self.destination_reached = False
-        # Q learning variables
-	self.alpha = 0.5
-	self.gamma = 0.1
-	self.epsilon = 0.1
-        self.actions = self.env.valid_actions
-	self.states = [(light, oncoming,left,right,next_waypoint)
-	    for light in ('red','green')
-            for oncoming in ('forward','left','right',None)
-            for left in ('forward','left','right',None)
-            for right in ('forward','left','right',None)		    
-	    for next_waypoint in ('forward','left','right')
-	           ]
-        self.Q = { s:[0]*len(self.actions) for s in self.states }
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -40,55 +26,54 @@ class LearningAgent(Agent):
         self.trial = 0
 	self.total_reward = 0
 	self.negative_reward = 0
-
+	self.destination_reached = False
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
-        # TODO: Update state
-        self.state = (inputs['light'], inputs['oncoming'], inputs['left'], inputs['right'], self.next_waypoint)
-	# TODO: Select action according to your policy
-	Qmax = self.Q[self.state].index(max(self.Q[self.state]))
+        # Follow traffic rules
+        action_okay = True
+        if self.next_waypoint == 'right':
+            if inputs['light'] == 'red' and inputs['left'] == 'forward':
+                action_okay = False
+        elif self.next_waypoint == 'forward':
+            if inputs['light'] == 'red':
+                action_okay = False
+        elif self.next_waypoint == 'left':
+            if inputs['light'] == 'red' or (inputs['oncoming'] == 'forward' or inputs['oncoming'] == 'right'):
+                action_okay = False
+        action = None
+        if action_okay:
+            action = self.next_waypoint
+        # TODO: Select action according to your policy
+        #action = random.choice([None,'forward','left','right'])
         
-	# Q learn or new path?
-	if random.uniform(0,1) < self.epsilon:
-	    action = random.choice(self.env.valid_actions)
-	else:
-	    action = self.actions[Qmax]
-        # Execute action and get reward
-	reward = self.env.act(self, action)
-
-	# Trial statistics
+	# Execute action and get reward
+        reward = self.env.act(self, action)
+        # Trial statistics
 	self.total_reward += reward
 	if reward < 0:
 	    self.negative_reward += reward
 	self.trial += 1
         self.destination_reached = reward > 2
+        
 	if self.destination_reached or deadline == 0: 
 	    self.save_trial_stats() 
- 
-	# Learn policy based on state, action and reward
-        # Q learn equation: Q(s,a) = (1-alpha)*Q(s,a) + alpha*(reward + gamma*argmaxQ(s',a'))
-	# get Q(s',a')
-        new_input = self.env.sense(self) 
-        new_next_waypoint = self.planner.next_waypoint()
-	new_state = (new_input['light'],new_input['oncoming'],new_input['left'],new_input['right'],new_next_waypoint)
-	
-        # Update the Q matrix
-	self.Q[self.state][self.actions.index(action)] = (1-self.alpha)*self.Q[self.state][self.actions.index(action)] + \
-	    (self.alpha*(reward + self.gamma*max(self.Q[new_state])))
-	
-       
+        # TODO: Learn policy based on state, action, reward
+
+       # print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
     def save_trial_stats(self):
+
         trial_df = pd.DataFrame([[self.total_reward, self.negative_reward, self.trial]],columns = ['total_reward','negative_reward','trial_length']) 
         if self.trial_stats.empty:
             self.trial_stats = trial_df
         else:
             self.trial_stats = self.trial_stats.append(trial_df, ignore_index = True)
         if self.trial_stats.shape[0] == 100:
-            self.trial_stats.to_csv('Q_learning_gamma'+str(self.gamma)+'.csv',index = False)
+            self.trial_stats.to_csv('planner_only.csv',index=False)
 
 
 def run():
@@ -97,7 +82,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
